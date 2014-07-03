83a6de890a433fe2c6371b9b367ecbeb0e5ee4b5
Slav <slav@kurilyak.com>
2014-07-03 01:42:17 -0700

    Add Access-Control-Allow-Origin policy

--- config/express.js
+++ config/express.js
@@ -11,6 +11,11 @@ module.exports = function(app, config) {
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
+    app.all('/*', function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "X-Requested-With");
+      next();
+    });
     app.use(app.router);
     app.use(function(req, res) {
       res.status(404).render('404', { title: '404' });
